6048e376320e5aaa6aa0e4f8b5871358d3491bc8
Jamey Sharp <jamey@minilop.net>
2019-02-01 17:14:50 +0100

    Another example, to move nodes within/between lists

---
+++ examples/moveafter.rs
@@ -0,0 +1,34 @@
+use acid_list::{AcidList, LinkIndex};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut args = std::env::args();
+    args.next(); // skip program name
+
+    let path = args.next().ok_or(io::ErrorKind::InvalidInput)?;
+
+    let from_idx = args
+        .next()
+        .ok_or(io::ErrorKind::InvalidInput)?
+        .parse()
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+
+    let kind = match &*args.next().ok_or(io::ErrorKind::InvalidInput)? {
+        "head" => LinkIndex::Head,
+        "node" => LinkIndex::Node,
+        other => return Err(io::Error::new(io::ErrorKind::InvalidInput, other)),
+    };
+
+    let to_idx = args
+        .next()
+        .ok_or(io::ErrorKind::InvalidInput)?
+        .parse()
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+
+    let file = std::fs::OpenOptions::new().read(true).write(true).open(path)?;
+    let mut list = AcidList::<[u8; 32]>::open(file)?;
+
+    list.move_after(from_idx, kind(to_idx));
+
+    list.close()
+}
